4ce76912de98dd117d27accbd7e1f8e45d812505
athimanshu <singh.himanshu86@gmail.com>
2018-07-17 22:35:44 +0530

    Spring Tutorial 06-Constructor Injection

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,16 +1,35 @@
 public class Triangle {
-    public String type;
+    private String type;
+
+    public int getHeight() {
+        return height;
+    }
+
+    private int height;
+    //Constructor Injection
+    Triangle(String type)
+    {
+        this.type=type;
+    }
+
+    public Triangle(String type, int height)
+    {
+        this.type=type;
+        this.height=height;
+    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+    //Commenting out the setter to use contructor injection
+//    public void setType(String type) {
+//        this.type = type;
+//    }
 
     public void draw()
     {
-        System.out.println(getType()+" Triangle drawn");
+        //System.out.println(getType()+" Triangle drawn");
+        System.out.println(getType() +" Triangle drawn of height "+ getHeight());
     }
 }
